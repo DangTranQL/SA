e9b56d78b739514914afa712b49634d2013a24f7
Tran <qt49@drexel.edu>
2025-09-03 17:43:56 -0400

    init fix

--- alg/mosa/main.py
+++ alg/mosa/main.py
@@ -3,8 +3,8 @@ from tqdm import tqdm
 from paretoset import paretoset
 import matplotlib.pyplot as plt
 import warnings
-import random
-from itertools import product
+# import random
+# from itertools import product
 from mosa import custom_mosa
 from eq import *
 
@@ -18,11 +18,6 @@ def run(runs=200):
         bounds = {'alpha': (0.01, 50), 'n': (0.01, 10)}
         objectives = compute
         
-        num_n = 10
-        num_alpha = 5*num_n
-        choices_n = np.linspace(bounds['n'][0]+0.05, bounds['n'][1]-0.05, num_n)
-        choices_alpha = np.linspace(bounds['alpha'][0]+0.05, bounds['alpha'][1]-0.05, num_alpha)
-        pairs = list(product(choices_alpha, choices_n))
 
     elif circuit == "posneg":
         params = ['beta_x', 'beta_y', 'n']
@@ -30,25 +25,20 @@ def run(runs=200):
         bounds = {'beta_x': (0.01, 50), 'beta_y': (0.01, 50), 'n': (0.01, 10)}
         objectives = compute
 
-        num_n = 10
-        num_beta_x = 5*num_n
-        num_beta_y = 5*num_n
-        choices_n = np.linspace(bounds['n'][0]+0.05, bounds['n'][1]-0.05, num_n)
-        choices_beta_x = np.linspace(bounds['beta_x'][0]+0.05, bounds['beta_x'][1]-0.05, num_beta_x)
-        choices_beta_y = np.linspace(bounds['beta_y'][0]+0.05, bounds['beta_y'][1]-0.05, num_beta_y)
-        pairs = list(product(choices_beta_x, choices_beta_y, choices_n))
+    
 
     with tqdm(total=runs, desc="Runs", position=0) as outer_pbar:
         for _ in range(runs):
-            choice = random.choice(pairs)
-            init = {key: choice[j] for j, key in enumerate(params)}
+            # choice = random.choice(pairs)
+            # init = {key: choice[j] for j, key in enumerate(params)}
             optimizer = custom_mosa()
             optimizer.setup(params, bounds, objectives, alpha=0.5)
-            optimizer.run(init, outer_pbar=outer_pbar)
+            optimizer.run(outer_pbar=outer_pbar)
             f1 = np.append(f1, optimizer.pareto_front[0]['f'][func_names[0]])
             f2 = np.append(f2, optimizer.pareto_front[0]['f'][func_names[1]])
+            optimizer.pareto_front = []
             outer_pbar.update(1)
-        
+
     mask = paretoset(np.column_stack((f1, f2)), sense=['min', 'min'])
     pareto_front = np.column_stack((f1[mask], f2[mask]))
 
--- alg/mosa/mosa.py
+++ alg/mosa/mosa.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import random
 
 class custom_mosa():
-    def __init__(self, initial_temp=300, final_temp=0.01, cooling_rate=0.95, num_iterations=500, step_size=0.05):
+    def __init__(self, initial_temp=1000, final_temp=0.001, cooling_rate=0.95, num_iterations=1000, step_size=0.2):
         self.initial_temp = initial_temp
         self.final_temp = final_temp
         self.cooling_rate = cooling_rate
@@ -17,50 +17,58 @@ class custom_mosa():
         self.objectives = objectives
         self.alpha = alpha
 
-    def run(self, vars_ini, outer_pbar=None):
+    def run(self, outer_pbar=None):
         temp = self.initial_temp
         # temps = [temp * (self.cooling_rate ** i) for i in range(self.num_temps)]
 
-        vars_curr = {key: np.random.uniform(*self.bounds[key]) for key in self.params}
-        # vars_curr = {key: vars_ini[key] for i, key in enumerate(self.params)}
-        f_curr = self.objectives([vars_curr[param] for param in self.params])
-        pareto = [{
-            'vars': {self.params[i]: vars_curr[self.params[i]] for i in range(len(self.params))},
-            'f': f_curr.copy()
-        }]
+        pmax = 0
+
         last_percent = 0
 
         with tqdm(total=100, desc="Temperatures", unit='%', leave=False, position=1) as iter_pbar:
             while temp >= self.final_temp:
                 # for temp in temps:
 
+                vars_curr = {key: np.random.uniform(*self.bounds[key]) for key in self.params}
+                # vars_curr = {key: vars_ini[key] for i, key in enumerate(self.params)}
+                f_curr = self.objectives([vars_curr[param] for param in self.params])
+                pareto = [{
+                    'vars': {self.params[i]: vars_curr[self.params[i]] for i in range(len(self.params))},
+                    'f': f_curr.copy()
+                }]
+
                 with tqdm(total=self.num_iterations, desc="Iterations", leave=False, position=2) as iter_pbar_2:
                     for _ in range(self.num_iterations):
                         gamma = 1
-                        p_list = []
 
                         vars_new = {key: np.clip(vars_curr[key] + np.random.uniform(-self.step_size, self.step_size), *self.bounds[key]) for i, key in enumerate(self.params)}
                         f_new = self.objectives([vars_new[param] for param in self.params])
 
                         for key in f_new:
                             if f_new[key] < f_curr[key]:
-                                p = 1
+                                pmax = p = 1
                             else:
                                 p = np.exp(-(f_new[key] - f_curr[key]) / temp)
-                                p_list.append(p)
+                                if pmax < p:
+                                    pmax = p    
+                            gamma *= p
 
-                        gamma = self.alpha * min(1, np.prod(p_list) if len(p_list) else 1) + (1-self.alpha) * min(1, max(p_list) if len(p_list) else 1)
+                        gamma = self.alpha * pmax + (1 - self.alpha) * gamma
 
                         if gamma == 1 or gamma > random.random():
                             vars_curr = vars_new
                             f_curr = f_new.copy()
 
-                            pareto = self._update_pareto(
-                                pareto,
-                                {self.params[i]: vars_new[self.params[i]] for i in range(len(self.params))},
-                                f_new.copy()
-                            )
-                    
+                            # pareto = self._update_pareto(
+                            #     pareto,
+                            #     {self.params[i]: vars_new[self.params[i]] for i in range(len(self.params))},
+                            #     f_new.copy()
+                            # )
+                            pareto.append({
+                                'vars': {self.params[i]: vars_new[self.params[i]] for i in range(len(self.params))},
+                                'f': f_new.copy()
+                            })
+
                         iter_pbar_2.update(1)
                 
                 temp *= self.cooling_rate
