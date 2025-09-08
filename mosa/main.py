17fd958ff34f84c2434ae2a25c18e80771a9fb00
Tran <qt49@drexel.edu>
2025-09-08 11:07:18 -0400

    random init

--- mosa/main.py
+++ mosa/main.py
@@ -1,63 +1,27 @@
-import numpy as np
-from tqdm import tqdm
-from paretoset import paretoset
-import matplotlib.pyplot as plt
-import warnings
-# import random
-# from itertools import product
 from mosa import custom_mosa
 from eq import *
 
-def run(runs=200):
-    f1 = np.array([])
-    f2 = np.array([])
-
+if __name__ == "__main__":
     if circuit == "neg":
-        params = ['alpha', 'n']
+        param_names = ['alpha', 'n']
         func_names = ['S_alpha', 'S_n']
         bounds = {'alpha': (0.01, 50), 'n': (0.01, 10)}
         objectives = compute
-        
+        data = np.load('data/ARneg_SensitivityPareto.npy')
 
     elif circuit == "posneg":
-        params = ['beta_x', 'beta_y', 'n']
+        param_names = ['beta_x', 'beta_y', 'n']
         func_names = [labels[choice1], labels[choice2]]
         bounds = {'beta_x': (0.01, 50), 'beta_y': (0.01, 50), 'n': (0.01, 10)}
         objectives = compute
+        data = np.load('data/PositiveNegative_SensitivityPairsParetos.npz')[f'paretoset_SensPair_{choice1}_{choice2}']
 
-        # num_n = 10
-        # num_beta_x = 5*num_n
-        # num_beta_y = 5*num_n
-        # choices_n = np.linspace(bounds['n'][0]+0.05, bounds['n'][1]-0.05, num_n)
-        # choices_beta_x = np.linspace(bounds['beta_x'][0]+0.05, bounds['beta_x'][1]-0.05, num_beta_x)
-        # choices_beta_y = np.linspace(bounds['beta_y'][0]+0.05, bounds['beta_y'][1]-0.05, num_beta_y)
-        # pairs = list(product(choices_beta_x, choices_beta_y, choices_n))
-
-    with tqdm(total=runs, desc="Runs", position=0) as outer_pbar:
-        for _ in range(runs):
-            # choice = random.choice(pairs)
-            # init = {key: choice[j] for j, key in enumerate(params)}
-            optimizer = custom_mosa()
-            optimizer.setup(params, bounds, objectives, alpha=0.5)
-            optimizer.run(outer_pbar=outer_pbar)
-            f1 = np.append(f1, optimizer.pareto_front[0]['f'][func_names[0]])
-            f2 = np.append(f2, optimizer.pareto_front[0]['f'][func_names[1]])
-            optimizer.pareto_front = []
-            outer_pbar.update(1)
-
-    mask = paretoset(np.column_stack((f1, f2)), sense=['min', 'min'])
-    pareto_front = np.column_stack((f1[mask], f2[mask]))
-
-    return pareto_front
-
-if __name__ == "__main__":
-    warnings.filterwarnings("ignore")
+    mosa = custom_mosa()
+    mosa.setup(param_names=param_names, bounds=bounds, func_names=func_names, objectives=compute, alpha=0.5)
 
     runs = int(input("\nEnter number of runs: "))
-    pareto_front = run(runs)
 
-    plt.scatter(pareto_front[:, 0], pareto_front[:, 1])
-    plt.xlabel("Objective 1")
-    plt.ylabel("Objective 2")
-    plt.title("Pareto Front")
-    plt.show()
+    mosa.run(runs=runs, use_tqdm=True)
+    mosa.prune()
+    mosa.plot()
+    mosa.gd_igd(ref=data)
--- mosa/mosa.py
+++ mosa/mosa.py
@@ -1,105 +1,144 @@
 import numpy as np
 from tqdm import tqdm
 import random
+from paretoset import paretoset
+from contextlib import contextmanager
+import matplotlib.pyplot as plt
+from pymoo.indicators.gd import GD
+from pymoo.indicators.igd import IGD
+import warnings
+
+warnings.filterwarnings("ignore")
+
+@contextmanager
+def dummy_tqdm(*args, **kwargs):
+    class Dummy:
+        def update(self, *a, **kw): pass
+        def close(self): pass
+    yield Dummy()
 
 class custom_mosa():
-    def __init__(self, initial_temp=1000, final_temp=0.001, cooling_rate=0.95, num_iterations=1000, step_size=0.2):
+    def __init__(self, initial_temp=1000, final_temp=0.001, cooling_rate=0.95, num_iterations=1000, step_size=0.1):
         self.initial_temp = initial_temp
         self.final_temp = final_temp
         self.cooling_rate = cooling_rate
         self.num_iterations = num_iterations
         self.step_size = step_size
-        self.pareto_front = []
 
-    def setup(self, params, bounds, objectives, alpha):
-        self.params = params
+    def setup(self, param_names, bounds, func_names, objectives, alpha):
+        self.param_names = param_names
         self.bounds = bounds
+        self.func_names = func_names
         self.objectives = objectives
         self.alpha = alpha
 
-    def run(self, outer_pbar=None):
-        temp = self.initial_temp
-        # temps = [temp * (self.cooling_rate ** i) for i in range(self.num_temps)]
+        self.pareto_front = []
+        self.func_1 = []
+        self.func_2 = []
+        self.params = {key: [] for key in self.param_names}
 
+    def run(self, runs=1, use_tqdm=True):
+        temp = self.initial_temp
         pmax = 0
+        f1_list, f2_list = [], []
 
         last_percent = 0
+        bar = tqdm if use_tqdm else dummy_tqdm
+        
+        with bar(total=runs, desc="Runs", position=0) as outer_pbar:
+            for _ in range(runs):
 
-        with tqdm(total=100, desc="Temperatures", unit='%', leave=False, position=1) as iter_pbar:
-            while temp >= self.final_temp:
-                # for temp in temps:
+                with bar(total=100, desc="Temperatures", unit='%', leave=False, position=1) as iter_pbar:
+                    while temp >= self.final_temp:
 
-                vars_curr = {key: np.random.uniform(*self.bounds[key]) for key in self.params}
-                # vars_curr = {key: vars_ini[key] for i, key in enumerate(self.params)}
-                f_curr = self.objectives([vars_curr[param] for param in self.params])
-                pareto = [{
-                    'vars': {self.params[i]: vars_curr[self.params[i]] for i in range(len(self.params))},
-                    'f': f_curr.copy()
-                }]
+                        with bar(total=self.num_iterations, desc="Iterations", leave=False, position=2) as iter_pbar_2:
+                            for _ in range(self.num_iterations):
+                                gamma = 1
 
-                with tqdm(total=self.num_iterations, desc="Iterations", leave=False, position=2) as iter_pbar_2:
-                    for _ in range(self.num_iterations):
-                        gamma = 1
+                                vars_curr = {key: np.random.uniform(*self.bounds[key]) for key in self.param_names}
+                                f_curr = self.objectives([vars_curr[param] for param in self.param_names])
 
-                        vars_new = {key: np.clip(vars_curr[key] + np.random.uniform(-self.step_size, self.step_size), *self.bounds[key]) for i, key in enumerate(self.params)}
-                        f_new = self.objectives([vars_new[param] for param in self.params])
+                                self.pareto_front.append({
+                                    'vars': {self.param_names[i]: vars_curr[self.param_names[i]] for i in range(len(self.param_names))},
+                                    'f': f_curr.copy()
+                                })
 
-                        for key in f_new:
-                            if f_new[key] < f_curr[key]:
-                                pmax = p = 1
-                            else:
-                                p = np.exp(-(f_new[key] - f_curr[key]) / temp)
-                                if pmax < p:
-                                    pmax = p    
-                            gamma *= p
+                                vars_new = {key: np.clip(vars_curr[key] + np.random.uniform(-self.step_size, self.step_size), *self.bounds[key]) for i, key in enumerate(self.param_names)}
+                                f_new = self.objectives([vars_new[param] for param in self.param_names])
 
-                        gamma = self.alpha * pmax + (1 - self.alpha) * gamma
+                                for key in f_new:
+                                    if f_new[key] < f_curr[key]:
+                                        pmax = p = 1
+                                    else:
+                                        p = np.exp(-(f_new[key] - f_curr[key]) / temp)
+                                        if pmax < p:
+                                            pmax = p    
+                                    gamma *= p
 
-                        if gamma == 1 or gamma > random.random():
-                            vars_curr = vars_new
-                            f_curr = f_new.copy()
+                                gamma = self.alpha * pmax + (1 - self.alpha) * gamma
 
-                            # pareto = self._update_pareto(
-                            #     pareto,
-                            #     {self.params[i]: vars_new[self.params[i]] for i in range(len(self.params))},
-                            #     f_new.copy()
-                            # )
-                            pareto.append({
-                                'vars': {self.params[i]: vars_new[self.params[i]] for i in range(len(self.params))},
-                                'f': f_new.copy()
-                            })
+                                if gamma == 1 or gamma > random.random():
+                                    vars_curr = vars_new
+                                    f_curr = f_new.copy()
 
-                        iter_pbar_2.update(1)
-                
-                temp *= self.cooling_rate
+                                    self.pareto_front.append({
+                                        'vars': {self.param_names[i]: vars_new[self.param_names[i]] for i in range(len(self.param_names))},
+                                        'f': f_new.copy()
+                                    })
 
-                percent_complete = (self.initial_temp - temp) / (self.initial_temp - self.final_temp) * 100
-                iter_pbar.update(percent_complete - last_percent)
-                last_percent = percent_complete
+                                iter_pbar_2.update(1)
+                        
+                        temp *= self.cooling_rate
 
-        self.pareto_front = pareto
+                        percent_complete = (self.initial_temp - temp) / (self.initial_temp - self.final_temp) * 100
+                        iter_pbar.update(percent_complete - last_percent)
+                        last_percent = percent_complete
 
-    def _dominates(self, f1, f2):
-        better_or_equal = all(f1[k] <= f2[k] for k in f1)
-        strictly_better = any(f1[k] < f2[k] for k in f1)
-        return better_or_equal and strictly_better
+                f1_list.extend(d["f"][self.func_names[0]] for d in self.pareto_front)
+                f2_list.extend(d["f"][self.func_names[1]] for d in self.pareto_front)
 
-    def _update_pareto(self, front, vars_dict, f_dict):
-        candidate = {
-            'vars': vars_dict.copy(),
-            'f': f_dict.copy()
-        }
-        new_front = []
-        dominated = False
+                for key in self.param_names:
+                    self.params[key].extend(d["vars"][key] for d in self.pareto_front)
 
-        for point in front:
-            if self._dominates(candidate['f'], point['f']):
-                continue
-            elif self._dominates(point['f'], candidate['f']):
-                dominated = True
-                new_front.append(point)
+                self.pareto_front = []
+                outer_pbar.update(1)
 
-        if not dominated:
-            new_front.append(candidate)
+        self.func_1 = np.asarray(f1_list, dtype=float)
+        self.func_2 = np.asarray(f2_list, dtype=float)
 
-        return new_front
+    def prune(self):
+        mask = paretoset(np.column_stack((self.func_1, self.func_2)), sense=['min', 'min'])
+
+        self.param_space = np.column_stack([np.array(v)[mask] for v in self.params.values()])
+        self.pareto_front = np.column_stack((self.func_1[mask], self.func_2[mask]))
+
+    def plot(self):
+        fig = plt.figure(figsize=(20, 10))
+        ax2d = fig.add_subplot(1, 2, 1)
+
+        ax2d.scatter(self.pareto_front[:, 0], self.pareto_front[:, 1])
+        ax2d.set_title("Pareto Plot")
+        ax2d.set_xlabel(self.func_names[0])
+        ax2d.set_ylabel(self.func_names[1])
+
+        if len(self.param_names) == 2:
+            ax2d2 = fig.add_subplot(1, 2, 2)
+            ax2d2.scatter(self.param_space[:, 0], self.param_space[:, 1])
+            ax2d2.set_title("Parameter Space")
+            ax2d2.set_xlabel(self.param_names[0])
+            ax2d2.set_ylabel(self.param_names[1])
+        else:
+            ax_3d = fig.add_subplot(1, 2, 2, projection='3d')
+            ax_3d.scatter(self.param_space[:, 0], self.param_space[:, 1], self.param_space[:, 2])
+            ax_3d.set_xlabel(self.param_names[0])
+            ax_3d.set_ylabel(self.param_names[1])
+            ax_3d.set_zlabel(self.param_names[2])
+            ax_3d.grid(True)
+            ax_3d.set_box_aspect([1, 1, 1])
+
+        plt.tight_layout()
+        plt.show()
+
+    def gd_igd(self, ref):
+        ind = GD(ref)
+        print("\nGeneration Distance = ", ind(self.pareto_front))
